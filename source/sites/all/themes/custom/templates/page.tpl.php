39e05f0b9d9c9bfa0a41c182de42befaa0dc4e77
Ricardo Pandales <ricardopandales@gmail.com>
2013-11-12 16:03:11 -0500

    Sync

--- source/sites/all/themes/custom/templates/page.tpl.php
+++ source/sites/all/themes/custom/templates/page.tpl.php
@@ -10,163 +10,98 @@
 
 <div id="page">
 
-  <header class="header" id="header" role="banner">
+    <header class="header" id="header" role="banner">
+        <div class="logo-wrapper">
+         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="sprite sprite-logo"
+           id="logo"></a>
+        </div>
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"></a>
-    <?php endif; ?>
-    
-    <figure class="tools">
-    	<img src="" />
-    </figure>
-    
-    <div class="contact_info">
-    	<div class="social">
-    		<a target="_blank" class="facebook" href="#"></a>
-    		<a target="_blank" class="twitter" href="#"></a>
-    		<a target="_blank" class="linkedin" href="#"></a>
-    	</div>
-    </div>
-    
-    <nav>
-    	<ul>
-    		<li><a href="#">¿quién eres?</a></li>
-    		<li><a href="#">estudio</a></li>
-    		<li><a href="#">proyectos</a></li>
-    		<li><a href="#">clientes</a></li>
-    		<li><a href="#">blog</a></li>
-    		<li><a href="#">probono*</a></li>
-    	</ul>
-    </nav>
-    
-    <aside>
-    	<a href="#">clientes como tú</a>
-    	<a href="#">otros clientes</a>
-    </aside>
-    
-    <div id="content">
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    	<article>
-    		<figure>
-    			<img src="" alt="" title="" />
-    		</figure>
-    	</article>
-    </div>
 
- <!--  backup  <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+        <div class="garden-tools">
+            <div class="sprite sprite-header_garden_tools"></div>
+        </div>
+
+        <div class="contact_info">
+            <div class="social">
+                <a target="_blank" class="sprite sprite-facebook_icon" href="#"></a>
+                <a target="_blank" class="sprite sprite-twitter_icon" href="#"></a>
+                <a target="_blank" class="sprite sprite-linked_in_icon" href="#"></a>
+                <div style="clear:both;float:none;"></div>
+            </div>
+
+        </div>
+        <div style="clear:both;float:none;"></div>
+        <?php print render($page['header']); ?>
+
+    </header>
+
+
+
+    <div id="main">
+
+        <div id="content" class="column" role="main">
+            <?php print render($page['highlighted']); ?>
+            <?php print $breadcrumb; ?>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+                <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php print render($tabs); ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php print render($page['content']); ?>
+            <?php print $feed_icons; ?>
+        </div>
+
+        <div id="navigation">
+
+            <?php if ($main_menu): ?>
+                <nav id="main-menu" role="navigation" tabindex="-1">
+                    <?php
+                    // This code snippet is hard to modify. We recommend turning off the
+                    // "Main menu" on your sub-theme's settings form, deleting this PHP
+                    // code block, and, instead, using the "Menu block" module.
+                    // @see https://drupal.org/project/menu_block
+                    print theme('links__system_main_menu', array(
+                        'links' => $main_menu,
+                        'attributes' => array(
+                            'class' => array('links', 'inline', 'clearfix'),
+                        ),
+                        'heading' => array(
+                            'text' => t('Main menu'),
+                            'level' => 'h2',
+                            'class' => array('element-invisible'),
+                        ),
+                    )); ?>
+                </nav>
+            <?php endif; ?>
+
+            <?php print render($page['navigation']); ?>
+
+        </div>
+
+        <?php
+        // Render the sidebars to see if there's anything in them.
+        $sidebar_first = render($page['sidebar_first']);
+        $sidebar_second = render($page['sidebar_second']);
+        ?>
+
+        <?php if ($sidebar_first || $sidebar_second): ?>
+            <aside class="sidebars">
+                <?php print $sidebar_first; ?>
+                <?php print $sidebar_second; ?>
+            </aside>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div>
- <?php endif; ?>
-
-
-
-    <?php print render($page['header']); ?>
-
-  </header>
-
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div>
 
-    <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
-
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
-
-  </div>
-
-  <?php print render($page['footer']); ?>
+    <?php print render($page['footer']); ?>
 
 </div>
 
-<?php print render($page['bottom']); ?>-->
+<?php print render($page['bottom']); ?>
